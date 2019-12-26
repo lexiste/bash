2adeb9b7e2f9a707514a08dfb09a5cff956d683c
root <root@r2d2.home.local>
2019-12-26 16:04:28 -0500

    update pass vendor, generate random mac for vendor

--- random-mac.sh
+++ random-mac.sh
@@ -3,16 +3,29 @@
 set -o errexit
 set -o nounset
 if [ $# == 0 ]; then
-  echo -e """Reset the MAC address on the given interface\n
-  Usage: $0 <interface>\n"""
+  echo -e """Reset the MAC address on the given interface
+  Usage: $0 <interface> <vendor>
+  use macchange --list to see all vendors supported"""
 
   exit 1
 fi
 
 _iface=$1
-/usr/bin/macchanger -s "${_iface}" # show current MAC addr before chaning
+_vendor=$2
+
+_rndVendor=$(macchanger --list | grep "${_vendor}" | shuf -n 1 | awk '{print $3}')
+_rndUniq=$(echo $RANDOM | md5sum | sed 's/.\{2\}/&:/g' | cut -c 1-8)
+_fullMac="$_rndVendor:$_rndUniq"
+
+# print out some of the generated info
+#echo -e "V:${_vendor}\t${_rndVendor}\t${_rndUniq}\t${_fullMac}"
+
+/usr/bin/macchanger --show "${_iface}" # show current MAC addr before chaning
 /sbin/dhclient -r "${_iface}" # release and stop running DHCP client
 /sbin/ifconfig "${_iface}" down # shutdown interface
-/usr/bin/macchanger -A "${_iface}" # randomize a new MAC addr on interface
+echo ""
+/usr/bin/macchanger --mac "${_fullMac}" "${_iface}" # randomize a new MAC addr on interface
 /sbin/ifconfig "${_iface}" up # bring interface up
 /sbin/dhclient ${_iface} # request new DHCP address
+echo ""
+/sbin/ip address show "${_iface}"
