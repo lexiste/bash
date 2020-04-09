bb05a33370c70b9917219e1d33c7455100d7d9ce
todd <todd@kali.local>
2020-04-09 07:31:36 -0400

    modified logging and display during runtime to show info to user

--- auto-blocking.sh
+++ auto-blocking.sh
@@ -41,7 +41,12 @@
 ##  where some issues with having `set -eu` and files/folders not existing with
 ##  error handling and exiting which needed fixed
 ##
-VERSION="0.3.1"
+## 2020-04-09
+##  changed the output to use tee (-a) command so there is feedback during
+##  execution, and not just a blank screen
+##
+
+VERSION="0.3.2"
 
 set -eu # exit on error or unset variables
 
@@ -59,16 +64,17 @@ GOOD='\e[92m' #green fg / no bg (default)
 init() {
 	# check if the base folder exists, create if not
   if [ ! -d "${folder}" ]; then
-     echo -e "${ALRT}[!!] Creating folder ${folder}${RST}"
+     echo -e "${ALRT}[!!] Creating folder ${folder}${RST}" | tee -a $logfile
      mkdir -p ${folder}
   fi
 	# check for log file to write to, create if not
 	if [ ! -f "${logfile}" ]; then
 		touch ${logfile}
+    echo -e "[${GOOD}+${RST}] created $logfile"
 	fi
 
   ## redirect stdout/stderr to file
-	exec &> $logfile
+	#exec &> $logfile
 }
 
 header() {
@@ -81,27 +87,27 @@ spamhaus file: ${spamhaus}
 bogon file: ${bogons}
 talos file: ${talos} [for record keeping]
 tor exit nodes file: ${torNodes} [for record keeping]
-----------------------------------------\n"""
+----------------------------------------\n""" | tee -a $logfile
 }
 
 main() {
   if [ -f "${spamhaus}" ]; then
-    echo -e "move ${GOOD}${spamhaus}${RST} to ${GOOD}${spamhaus}.$(date +%d%b)${RST}"
+    echo -e "move ${GOOD}${spamhaus}${RST} to ${GOOD}${spamhaus}.$(date +%d%b)${RST}" | tee -a $logfile
     mv --force ${spamhaus} ${spamhaus}.$(date +%d%b)
   fi
 
   if [ -f "${bogons}" ]; then
-    echo -e "move ${GOOD}${bogons}${RST} to ${GOOD}${bogons}.$(date +%d%b)${RST}"
+    echo -e "move ${GOOD}${bogons}${RST} to ${GOOD}${bogons}.$(date +%d%b)${RST}" | tee -a $logfile
     mv --force ${bogons} ${bogons}.$(date +%d%b)
   fi
 
   if [ -f "${talos}" ]; then
-    echo -e "move ${GOOD}${talos}${RST} to ${GOOD}${talos}.$(date +%d%b)${RST}"
+    echo -e "move ${GOOD}${talos}${RST} to ${GOOD}${talos}.$(date +%d%b)${RST}" | tee -a $logfile
     mv --force ${talos} ${talos}.$(date +%d%b)
   fi
 
   if [ -f "${torNodes}" ]; then
-    echo -e "move ${GOOD}${torNodes}${RST} to ${GOOD}${torNodes}.$(date +%d%b)${RST}"
+    echo -e "move ${GOOD}${torNodes}${RST} to ${GOOD}${torNodes}.$(date +%d%b)${RST}" | tee -a $logfile
     mv --force ${torNodes} ${torNodes}.$(date +%d%b)
   fi
 
@@ -110,61 +116,61 @@ main() {
   wget --timeout=20 --quiet -O ${spamhaus} https://spamhaus.org/drop/drop.lasso
   # checking file size > 0, better than simple if it exists
   if [ -s ${spamhaus} ]; then
-    echo -e "${GOOD}[+]${RST} downloading ${spamhaus} file"
+    echo -e "${GOOD}[+]${RST} downloading ${spamhaus} file" | tee -a $logfile
   else
-    echo -e "${ALRT}[-]${RST} downloading ${spamhaus} file"
+    echo -e "${ALRT}[-]${RST} downloading ${spamhaus} file" | tee -a $logfile
   fi
 
   wget --timeout=20 --quiet -O ${bogons} https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt
   # checking file size > 0, better than simple if it exists
   if [ -s ${bogons} ]; then
-    echo -e "${GOOD}[+]${RST} downloading ${bogons} file"
+    echo -e "${GOOD}[+]${RST} downloading ${bogons} file" | tee -a $logfile
   else
-    echo -e "${ALRT}[-]${RST} downloading ${bogons} file"
+    echo -e "${ALRT}[-]${RST} downloading ${bogons} file" | tee -a $logfile
   fi
 
   wget --timeout=20 --quiet -O ${talos} https://talosintelligence.com/documents/ip-blacklist
   # checking file size > 0, better than simple if it exists
   if [ -s "${talos}" ]; then
-    echo -e "${GOOD}[+]${RST} downloading ${talos} file"
+    echo -e "${GOOD}[+]${RST} downloading ${talos} file" | tee -a $logfile
   else
-    echo -e "${ALRT}[-]${RST} downloading ${talos} file"
+    echo -e "${ALRT}[-]${RST} downloading ${talos} file" | tee -a $logfile
   fi
 
   wget --quiet -O ${torNodes} https://check.torproject.org/exit-addresses
   if [ -s "${torNodes}" ]; then
-    echo -e "${GOOD}[+]${RST} downloading ${torNodes} file"
+    echo -e "${GOOD}[+]${RST} downloading ${torNodes} file" | tee -a $logfile
   else
-    echo -e "${ALRT}[-]${RST} downloading ${torNodes} file"
+    echo -e "${ALRT}[-]${RST} downloading ${torNodes} file" | tee -a $logfile
   fi
 
   echo "Completed downloading files ... processing and loading into iptables..."
 
   if [ ! -s "${spamhaus}" ]; then
-     echo -e "${ALRT}[!!]${RST} unable to find drop list file ${spamhaus}"
-     echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O ${spamhaus}"
+     echo -e "${ALRT}[!!]${RST} unable to find drop list file ${spamhaus}" | tee -a $logfile
+     echo -e "perhaps do: wget https://spamhaus.org/drop/drop.lasso -O ${spamhaus}" | tee -a $logfile
      exit 1
   else
      # convert the spamhaus file into a Cisco formated ACL file that c/would be used to update router(s)
-     echo -e "converting ${spamhaus} into ACL format files"
+     echo -e "converting ${spamhaus} into ACL format files" | tee -a $logfile
      cat ${spamhaus} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $spamhaus.in
      cat ${spamhaus} | grep -v "^;" | awk 'BEGIN {FS=" ; "}; {print $1}' | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip any "$1" "$2}' > $spamhaus.out
-     echo -e "finished processing ${spamhaus}\n"
+     echo -e "finished processing ${spamhaus}\n" | tee -a $logfile
   fi
 
   if [ ! -s "${bogons}" ]; then
-     echo -e "${ALRT}[!!]${RST} unable to find the bogons file $bogons"
-     echo -e "perhaps a visit to https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
+     echo -e "${ALRT}[!!]${RST} unable to find the bogons file $bogons" | tee -a $logfile
+     echo -e "perhaps a visit to https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt" | tee -a $logfile
      exit 1
   else
      # convert the bogons file into a Cisco formated ACL file that c/would be used to update router(s)
-     echo -e "converting ${bogons} into ACL format files"
+     echo -e "converting ${bogons} into ACL format files" | tee -a $logfile
      cat ${bogons} | grep -v "^#" | sed -f ~/scripts/subnet2mask.sed | awk '{ print "deny ip "$1" "$2" any"}' > $bogons.in
-     echo -e "finished processing ${bogons}\n"
+     echo -e "finished processing ${bogons}\n" | tee -a $logfile
   fi
 
   if [ ! -x /sbin/iptables ]; then
-     echo -e "${ALRT}[!!]${RST} missing iptables command line tool, exiting"
+     echo -e "${ALRT}[!!]${RST} missing iptables command line tool, exiting" | tee -a $logfile
      exit 1
   fi
 
@@ -173,7 +179,7 @@ main() {
   ##  in a prod world creating a backup, or configuration of other needed (ie. required)
   ##  rules would be in another file to be loaded as well
   ##  (EX: block telnet in/out, whitelist known hosts, etc.)
-  echo -e "${ALRT}[!!]${RST} purging previous iptables rules..."
+  echo -e "${ALRT}[!!]${RST} purging previous iptables rules..." | tee -a $logfile
   sudo /sbin/iptables -P INPUT ACCEPT
   sudo /sbin/iptables -P FORWARD ACCEPT
   sudo /sbin/iptables -P OUTPUT ACCEPT
@@ -183,7 +189,7 @@ main() {
   sudo /sbin/iptables -X
 
   ## looks like we have the input file and iptables located
-  echo -e "${GOOD}loading${RST} $spamhaus into iptables..."
+  echo -e "${GOOD}loading${RST} $spamhaus into iptables..." | tee -a $logfile
   cat "${spamhaus}" \
    | sed -e 's/;.*//' \
    | grep -v '^ *$' \
