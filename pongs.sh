bef9e9bf77a8aa82cff46f5b4bcf81f56b320368
todd <todd@kali.local>
2020-04-10 10:23:39 -0400

    house keeping for stupid windows file handling

--- pongs.sh
+++ pongs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-_r='\033[0m'    #reset
+_r='\033[0m'    # reset
 _a='\033[1;91m' # Bold light Red FG / default bg
 _g='\033[1;92m' # Bold light green FG / default bg
 _c='\033[33m'   # Yellow FG
@@ -11,18 +11,24 @@ if ! [ -r "$1" ]; then
   exit -1
 fi
 
-domains=(".gspt.net" ".gsiccorp.net" ".innotrac.com")
+domains=("gspt.net" "gsiccorp.net" "innotrac.com" "prd.gsi.local")
+
 while read name; do
-  for i in ${!domains[*]}
-  do
-    host="$name${domains[$i]}"
-#    fping --alive --addr --name $host
-    ping -c1 -q -w 2 $host &>/dev/null
+  ## when importing a file from windows, we need to trim the trailing line feed
+  clean=$(tr -d '\n' <<< $name)
+  clean=$(tr -d '\r' <<< $clean)
+#  echo -e "[n] $clean"
+  for dom in ${domains[*]}; do
+    fqdn="${clean}.${dom}"
+#    echo -e " [d]\t$dom"
+#    echo -e " [fqdn]\t$fqdn"
+
+    ping -c1 -q -w 2 $fqdn &>/dev/null
     if [ $? -eq 0 ]; then
-      ip=$(nslookup $host | awk -F": " '/Address/{print $2}' | tr '\n' ' ')
-      echo $host $ip
+      ip=$(nslookup $fqdn | awk -F": " '/Address/{print $2}' | tr '\n' ' ')
+      echo $fqdn $ip
 #    else
-#      echo $host not found
+#      echo $fqdn not found
     fi
   done
 done < "$1"
