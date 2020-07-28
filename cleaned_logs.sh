0e8185520ace492f2675c24bd1f679441929c5f9
todd <todd@kali.local>
2020-07-28 16:45:53 -0400

    minor updates around output display

--- cleaned_logs.sh
+++ cleaned_logs.sh
@@ -4,56 +4,52 @@
 # based on https://www.sandflysecurity.com/blog/using-linux-utmpdump-for-forensics-and-detecting-log-file-tampering/
 # article for reviewing [b|u|w]tmp files to see if they were tampered with
 
-#
-# like Colour
-#
-RED=`echo -e "\033[31m"`
-LCYAN=`echo -e "\033[36m"`
-YELLOW=`echo -e "\033[33m"`
-GREEN=`echo -e "\033[32m"`
-BLUE=`echo -e "\033[34m"`
-NORMAL=`echo -e "\033[m"`
+coltable="/home/todd/scripts/bash/COL_TABLE"
+if [[ -f ${coltable} ]]; then
+  source ${coltable}
+fi
 
-VERSION="0.1"
-AUTHOR="todd fencl"
+readonly VERSION="0.1"
+readonly AUTHOR="todd fencl"
+
 header() {
   clean
   echo -e """
 ----------------------------------------
 Check [b|u|w]tmp for null'd entries
-Run Date: ${GREEN}$(date +%d-%b-%Y\ %H:%M)${NORMAL}
-Version: ${YELLOW}${VERSION}${NORMAL} by: ${AUTHOR}
+Run Date: ${COL_GREEN}$(date +%d-%b-%Y\ %H:%M)${COL_NC}
+Version: ${COL_YELLOW}${VERSION}${COL_NC} by: ${AUTHOR}
 ----------------------------------------\n"""
 }
 
 main() {
   if [ ! -x /usr/bin/utmpdump ]; then
-    echo -e "${RED}[!!]${NORMAL} -- missing utmpdump utility"
+    echo -e "${CROSS} -- missing utmpdump utility"
     exit 1
   fi
 
   # check if utmp exists
   if [ -f "/var/run/utmp" ]; then
-    echo -e "----------\nChecking ${LCYAN}UTMP${NORMAL}"
+    echo -e "----------\nChecking ${LCYAN}UTMP${COL_NC}"
     /usr/bin/utmpdump /var/run/utmp | grep "\[0\]/*1970-01-01"
   else
-    echo -e "${RED}[!!]${NORMAL} -- missing /var/run/utmp file"
+    echo -e "${CROSS} -- missing /var/run/utmp file"
   fi
 
   # check if wtmp exists
   if [ -f "/var/log/wtmp" ]; then
-    echo -e "----------\nChecking ${LCYAN}WTMP${NORMAL}"
+    echo -e "----------\nChecking ${LCYAN}WTMP${COL_NC}"
     /usr/bin/utmpdump /var/log/wtmp | grep "\[0\]/*1970-01-01"
   else
-    echo -e "${RED}[!!]${NORMAL} -- missing /var/log/wtmp file"
+    echo -e "${CROSS} -- missing /var/log/wtmp file"
   fi
 
   # check if btmp exists
   if [ -f "/var/log/btmp" ]; then
-    echo -e "----------\nChecking ${LCYAN}BTMP${NORMAL}"
+    echo -e "----------\nChecking ${LCYAN}BTMP${COL_NC}"
     /usr/bin/utmpdump /var/log/btmp | grep "\[0\]/*1970-01-01"
   else
-    echo -e "${RED}[!!]${NORMAL} -- missing /var/log/btmp file"
+    echo -e "${CROSS} -- missing /var/log/btmp file"
   fi
 }
 
